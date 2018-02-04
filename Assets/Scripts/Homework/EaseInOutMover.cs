eb2b4543cf2832298d29e2f86d6dffad995783ab
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-02-04 12:52:28 -0500

    Created the Ease In-Out animation.

--- Assets/Scripts/Homework/EaseInOutMover.cs
+++ Assets/Scripts/Homework/EaseInOutMover.cs
@@ -1,14 +1,33 @@
 ﻿using UnityEngine;
 
-public class EaseInOutMover : MonoBehaviour {
+public class EaseInOutMover : MonoBehaviour
+{
+    public Bounds bounds;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x >= bounds.maxX)
+        {
+            rigidBody.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            if (rigidBody.position.x <= 0)
+            {
+                rigidBody.AddForce(new Vector2(0.00060f, 0));
+            }
+            else
+            {
+                rigidBody.AddForce(new Vector2(-0.00060f, 0));
+            }
+        }
+    }
 }
