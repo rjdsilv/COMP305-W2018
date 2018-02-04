bca577b60bca2f454a2bd546864309d9f4d598a6
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-02-04 12:49:53 -0500

    Created Ease In and Ease Out animations

---
+++ Assets/Scripts/Homework/EaseInMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EaseInMover : MonoBehaviour
+{
+    public Bounds bounds;
+
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x >= bounds.maxX)
+        {
+            rigidBody.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            rigidBody.AddForce(new Vector2(0.00030f, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/Homework/EaseOutMover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EaseOutMover : MonoBehaviour
+{
+    public Bounds bounds;
+
+    private Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(9.85f, 0);
+    }
+
+    void FixedUpdate ()
+    {
+        if (transform.position.x >= bounds.maxX)
+        {
+            rigidBody.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            if (rigidBody.velocity.x > 0)
+            {
+                rigidBody.AddForce(new Vector2(-0.00030f, 0));
+            }
+        }
+    }
+}
