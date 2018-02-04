c02180e60fccad096e5854c5b2a414ddd346b4f4
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-02-04 12:26:41 -0500

    Created the Lab02

---
+++ Assets/Scripts/Homework/LinearMover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LinearMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Labs/Lab02/SpriteChanger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public Sprite playerSprite;
+    public GameObject playerGameObject;
+
+    void OnMouseDown()
+    {
+        playerGameObject.GetComponent<SpriteRenderer>().sprite = playerSprite;
+        playerGameObject.transform.localScale = new Vector3(3, 3, 0);
+    }
+}
