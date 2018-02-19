395181968230b3f33812c83f3d0898a7c9f67e1d
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2018-02-19 16:49:13 -0500

    Implemented the clear.

---
+++ Assets/Scripts/Homework/Animation/Path10Points/Path10Clear.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Path10Clear : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<Path10Points>().ClearAndReset();
+    }
+}
--- Assets/Scripts/Homework/Animation/Path10Points/Path10Points.cs
+++ Assets/Scripts/Homework/Animation/Path10Points/Path10Points.cs
@@ -8,23 +8,24 @@ public class Path10Points : MonoBehaviour
     public GameObject startButton;
     public GameObject pathWalker;
 
-    private int fromPositionIndex = 0;
-    private int toPositionIndex = 1;
+    private int _fromPositionIndex = 0;
+    private int _toPositionIndex = 1;
     private bool _startWalk = false;
     private GameObject walker;
     private List<Vector3> _clickecPositions = new List<Vector3>();
+    private List<Transform> _pathPoints = new List<Transform>();
 
     private void FixedUpdate()
     {
         if (_startWalk)
         {
-            if ((walker.transform.position - _clickecPositions[toPositionIndex]).magnitude <= 0.025f)
+            if ((walker.transform.position - _clickecPositions[_toPositionIndex]).magnitude <= 0.05f)
             {
-                if (toPositionIndex < _clickecPositions.Count - 1)
+                if (_toPositionIndex < _clickecPositions.Count - 1)
                 {
-                    fromPositionIndex++;
-                    toPositionIndex++;
-                    walker.GetComponent<Rigidbody2D>().velocity = (_clickecPositions[toPositionIndex] - _clickecPositions[fromPositionIndex]).normalized * speed;
+                    _fromPositionIndex++;
+                    _toPositionIndex++;
+                    walker.GetComponent<Rigidbody2D>().velocity = (_clickecPositions[_toPositionIndex] - _clickecPositions[_fromPositionIndex]).normalized * speed;
                 }
                 else
                 {
@@ -41,7 +42,7 @@ public class Path10Points : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 Vector3 clickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 10;
-                Instantiate(pathPoint, clickedPosition, Quaternion.identity);
+                _pathPoints.Add(Instantiate(pathPoint, clickedPosition, Quaternion.identity));
                 _clickecPositions.Add(clickedPosition);
 
                 if (_clickecPositions.Count == 3)
@@ -55,7 +56,21 @@ public class Path10Points : MonoBehaviour
     public void StartWalk()
     {
         _startWalk = true;
-        walker = Instantiate(pathWalker, _clickecPositions[fromPositionIndex], Quaternion.identity);
-        walker.GetComponent<Rigidbody2D>().velocity = (_clickecPositions[toPositionIndex] - _clickecPositions[fromPositionIndex]).normalized * speed;
+        walker = Instantiate(pathWalker, _clickecPositions[_fromPositionIndex], Quaternion.identity);
+        walker.GetComponent<Rigidbody2D>().velocity = (_clickecPositions[_toPositionIndex] - _clickecPositions[_fromPositionIndex]).normalized * speed;
+    }
+
+    public void ClearAndReset()
+    {
+        Destroy(walker);
+        foreach (Transform t in _pathPoints)
+        {
+            Destroy(t.gameObject);
+        }
+        _startWalk = false;
+        _pathPoints.Clear();
+        _clickecPositions.Clear();
+        _fromPositionIndex = 0;
+        _toPositionIndex = 1;
     }
 }
